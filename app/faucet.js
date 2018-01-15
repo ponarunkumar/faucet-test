5107e44140afd43d5a68562f3e9607c7f53dee29
Pon ArunKumar Ramalingam <ponarunkumar@gmail.com>
2018-01-15 10:28:38 -0500

    typeof check

--- app/faucet.js
+++ app/faucet.js
@@ -1,5 +1,5 @@
 
-if(typeof web3 !== undefined) {
+if(typeof web3 !== 'undefined') {
 	web3 = new Web3(web3.currentProvider);
 } else { 
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
